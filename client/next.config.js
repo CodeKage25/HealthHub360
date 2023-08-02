9f7bdc6da8370dbc573b6ebe86cba1d659238e0a
Babatunde Abdulkareem <babzchizzy27@gmail.com>
2023-08-02 15:55:30 +0100

    update

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,6 @@
 const basePath = process.env.NODE_ENV === 'production' ? '/client' : '';
 const nextConfig = {
     basePath: basePath, // The path to the client folder
-  distDir: 'build', 
 }
 
 module.exports = nextConfig
